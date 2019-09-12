b0fe72bb1631b78edb59c8da2dcf6ed10a4cfbca
Thamisitech <50879028+Thamisitech@users.noreply.github.com>
2019-09-12 11:14:10 -0400

    quiz2 start!

---
+++ app.js
@@ -0,0 +1,5 @@
+// John and Mike both play basketball in different teams. In the latest 3 games, John's team scored 89, 120 and 103 points, while Mike's team scored 116, 94 and 123 points.
+
+// 1. Calculate the average score for each team
+// 2. Decide which teams wins in average (highest average score), and print the winner to the console. Also include the average score in the output.
+// 3. Then change the scores to show different winners. Don't forget to take into account there might be a draw (the same average score)
